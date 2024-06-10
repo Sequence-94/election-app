cd95f67fc65fe53340aa9a7c9d330ea07807e674
Sequence-94 <luvolwethu14@icloud.com>
2024-06-10 11:52:57 +0200

    Updated Election contract with candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -3,11 +3,26 @@ pragma solidity >=0.4.22 <0.8.0;
 
 //Declare a contract
 contract Election {
-    //Store candidate
-    //Read candidate
-    string public candidate;
-    //constructor to initialize contract upon migration
+    //Model a candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    //Store candidates
+    //Fetch candidate
+    mapping(uint => Candidate) public candidates;
+    //Store candidate count
+    uint public candidatesCount;
+
     constructor() public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    //add a new candidate
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
