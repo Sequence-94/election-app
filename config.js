4ad19f78b14ac12c728c5323c2931791d895cdaa
Sequence-94 <luvolwethu14@icloud.com>
2024-06-14 19:22:18 +0200

    added config.js file

---
+++ config.js
@@ -0,0 +1,13 @@
+const awsconfig = {
+    API: {
+        endpoints: [
+            {
+                name: "web3",
+                endpoint:
+                    "https://lobpqub6fb.execute-api.us-east-1.amazonaws.com/prod/",
+            },
+        ],
+    },
+}
+
+export default awsconfig
